d0f34784a29fffb2b4b4cd7b170f7c4c4269511b
Paweł Bednarz <pbedn@users.noreply.github.com>
2025-03-26 13:58:17 +0100

    Add pause function

--- game.c
+++ game.c
@@ -1,11 +1,16 @@
+#include <iso646.h>
+
 #include "raylib.h"
 #include "raymath.h"
 #include "resource_dir.h"
+
+#define DEV_MODE
 
 #define PADDLE_WIDTH 15
 #define PADDLE_HEIGHT 150
 #define BALL_SIZE 18
 #define BALL_SPEED 400.0f
+#define WIN_SCORE 10
 
 typedef struct Paddle {
     Rectangle rect;
@@ -14,15 +19,40 @@ typedef struct Paddle {
 
 typedef struct Ball {
     Vector2 position;
-    Vector2 direction; // Direction vector
-    float speed;       // Speed scalar
+    Vector2 direction;
+    float speed;
 } Ball;
+
+typedef struct {
+    Sound hit;
+    Sound edge;
+    Sound top;
+} Sounds;
+
+typedef enum {
+    MAIN_MENU,
+    PREGAME,
+    GAME,
+    GAME_OVER
+} Scene;
+
+typedef struct {
+    int leftScore;
+    int rightScore;
+    bool isPaused;
+    Scene currentScene;
+    Sounds sounds;
+} GameState;
 
 const int screenWidth = 1280;
 const int screenHeight = 720;
 
+void GameLogic(Paddle *leftPaddle, Paddle *rightPaddle, Ball *ball, GameState *state);
 void ResetBall(Ball *ball);
-void DrawDashedLine();
+void DrawDashedLine(Color color);
+void DrawMainMenu();
+void DrawGame();
+void DrawGameOver();
 
 // Function to reset the ball
 void ResetBall(Ball *ball) {
@@ -36,18 +66,29 @@ void ResetBall(Ball *ball) {
 }
 
 int main(void) {
+    SearchAndSetResourceDir("resources");
+
     SetTraceLogLevel( LOG_ALL );
     SetConfigFlags( FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "Pong Game");
     SetTargetFPS(60);
 
-    SearchAndSetResourceDir("resources");
-
-    InitAudioDevice();      // Initialize audio device
-
+    InitAudioDevice();
     Sound sn_beep = LoadSound("sounds/ping_pong_8bit_beeep.ogg");
     Sound sn_peep = LoadSound("sounds/ping_pong_8bit_peeeeeep.ogg");
     Sound sn_plop = LoadSound("sounds/ping_pong_8bit_plop.ogg");
+
+    GameState state = {
+        .leftScore = 0,
+        .rightScore = 0,
+        .isPaused = false,
+        .currentScene = MAIN_MENU,
+        .sounds = {.top = sn_beep, .edge = sn_peep, .hit = sn_plop},
+    };
+
+#ifdef DEV_MODE
+    state.currentScene = PREGAME;
+#endif
 
     Paddle leftPaddle = {{50, (int)((screenHeight / 2)) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT}, 500.0f};
     Paddle rightPaddle = {{screenWidth - 50 - PADDLE_WIDTH, (int)((screenHeight / 2)) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT}, 500.0f};
@@ -58,69 +99,90 @@ int main(void) {
     ball.direction.x /= length;
     ball.direction.y /= length;
 
-    // Score variables
-    int leftScore = 0;
-    int rightScore = 0;
-
     ResetBall(&ball); // Initial reset
-
     while (!WindowShouldClose()) {
-        // Update
-        if (IsKeyDown(KEY_W) && leftPaddle.rect.y > 0) {
-            leftPaddle.rect.y -= leftPaddle.speed * GetFrameTime();
-        }
-        if (IsKeyDown(KEY_S) && leftPaddle.rect.y < screenHeight - PADDLE_HEIGHT) {
-            leftPaddle.rect.y += leftPaddle.speed * GetFrameTime();
-        }
-        if (IsKeyDown(KEY_UP) && rightPaddle.rect.y > 0) {
-            rightPaddle.rect.y -= rightPaddle.speed * GetFrameTime();
-        }
-        if (IsKeyDown(KEY_DOWN) && rightPaddle.rect.y < screenHeight - PADDLE_HEIGHT) {
-            rightPaddle.rect.y += rightPaddle.speed * GetFrameTime();
-        }
-
-        // Update ball position
-        ball.position.x += ball.direction.x * ball.speed * GetFrameTime();
-        ball.position.y += ball.direction.y * ball.speed * GetFrameTime();
-
-        // Ball collision with top and bottom
-        if (ball.position.y <= 0 || ball.position.y >= screenHeight - BALL_SIZE) {
-            ball.direction.y *= -1; // Reverse Y direction
-            PlaySound(sn_beep);
-        }
-
-        // Ball collision with paddles
-        if (CheckCollisionCircleRec(ball.position, BALL_SIZE / 2, leftPaddle.rect) || 
-            CheckCollisionCircleRec(ball.position, BALL_SIZE / 2, rightPaddle.rect)) {
-            ball.direction.x *= -1; // Reverse X direction
-            ball.speed += BALL_SPEED / 10.0;
-            PlaySound(sn_plop);
-        }
-
-        // Scoring
-        if (ball.position.x < 0) {
-            PlaySound(sn_peep);
-            rightScore++; // Right player scores
-            ResetBall(&ball);  // Reset ball after scoring
-        }
-        if (ball.position.x > screenWidth) {
-            PlaySound(sn_peep);
-            leftScore++;  // Left player scores
-            ResetBall(&ball);  // Reset ball after scoring
+        switch (state.currentScene) {
+            case MAIN_MENU:
+                if (IsKeyPressed(KEY_ONE)) {
+                    state.currentScene = GAME;
+                }
+                else if (IsKeyPressed(KEY_TWO)) {
+                }
+                else if (IsKeyPressed(KEY_THREE)) {
+                    CloseWindow();
+                }
+                break;
+            case PREGAME:
+                if (IsKeyPressed(KEY_SPACE) or IsKeyPressed(KEY_ENTER))
+                    state.currentScene = GAME;
+                break;
+            case GAME:
+                if (!state.isPaused)
+                    GameLogic(&leftPaddle, &rightPaddle, &ball, &state);
+                if (IsKeyPressed(KEY_P)) {
+                    state.isPaused = !state.isPaused;
+                }
+                break;
+            case GAME_OVER:
+                ball.speed = 0.0f;
+                if (IsKeyPressed(KEY_R)) {
+                    state.leftScore = 0;
+                    state.rightScore = 0;
+                    ResetBall(&ball);
+                    state.currentScene = GAME;
+                }
+                else if (IsKeyPressed(KEY_M)) {
+                    state.currentScene = MAIN_MENU;
+                }
+                break;
+            default: break;
         }
 
-        // Draw
         BeginDrawing();
-        ClearBackground(DARKGRAY);
-        DrawRectangleRec(leftPaddle.rect, RAYWHITE);
-        DrawRectangleRec(rightPaddle.rect, RAYWHITE);
-        DrawCircleV(ball.position, BALL_SIZE / 2, RAYWHITE);
-        DrawDashedLine();
-
-        // Draw scores
-        DrawText(TextFormat("%d", leftScore), screenWidth / 4, 20, 80, RAYWHITE);
-        DrawText(TextFormat("%d", rightScore), 3 * screenWidth / 4, 20, 80, RAYWHITE);
-
+        switch (state.currentScene) {
+            case MAIN_MENU:
+                ClearBackground(DARKGRAY);
+                DrawText("Main Menu", GetScreenWidth() / 2 - MeasureText("Main Menu", 40) / 2, GetScreenHeight() / 2 - 80, 40, RAYWHITE);
+                DrawText("1. Play Game", GetScreenWidth() / 2 - MeasureText("1. Play Game", 20) / 2, GetScreenHeight() / 2, 20, RAYWHITE);
+                DrawText("2. Settings", GetScreenWidth() / 2 - MeasureText("2. Settings", 20) / 2, GetScreenHeight() / 2 + 40, 20, RAYWHITE);
+                DrawText("3. Exit", GetScreenWidth() / 2 - MeasureText("3. Exit", 20) / 2, GetScreenHeight() / 2 + 80, 20, RAYWHITE);
+                break;
+            case PREGAME:
+                ClearBackground(DARKGRAY);
+                DrawRectangleRec(leftPaddle.rect, LIGHTGRAY);
+                DrawRectangleRec(rightPaddle.rect, LIGHTGRAY);
+                DrawDashedLine(LIGHTGRAY);
+                DrawText(TextFormat("%d", state.leftScore), screenWidth / 4, 20, 80, LIGHTGRAY);
+                DrawText(TextFormat("%d", state.rightScore), 3 * screenWidth / 4, 20, 80, LIGHTGRAY);
+                DrawText(TextFormat("Press to start game."), (screenWidth / 2) - (screenWidth / 4), screenHeight / 3, 60, RAYWHITE);
+                break;
+            case GAME:
+                ClearBackground(DARKGRAY);
+                DrawRectangleRec(leftPaddle.rect, RAYWHITE);
+                DrawRectangleRec(rightPaddle.rect, RAYWHITE);
+                DrawCircleV(ball.position, BALL_SIZE / 2, RAYWHITE);
+                DrawDashedLine(RAYWHITE);
+                DrawText(TextFormat("%d", state.leftScore), screenWidth / 4, 20, 80, RAYWHITE);
+                DrawText(TextFormat("%d", state.rightScore), 3 * screenWidth / 4, 20, 80, RAYWHITE);
+                if (state.isPaused) {
+                    ClearBackground(DARKGRAY);
+                    DrawRectangleRec(leftPaddle.rect, LIGHTGRAY);
+                    DrawRectangleRec(rightPaddle.rect, LIGHTGRAY);
+                    DrawDashedLine(LIGHTGRAY);
+                    DrawText(TextFormat("Paused."), GetScreenWidth() / 2 - MeasureText("Paused", 60) / 2, screenHeight / 2, 60, RED);
+                    break;
+                }
+                break;
+            case GAME_OVER:
+                ClearBackground(DARKGRAY);
+                DrawRectangleRec(leftPaddle.rect, LIGHTGRAY);
+                DrawRectangleRec(rightPaddle.rect, LIGHTGRAY);
+                DrawDashedLine(LIGHTGRAY);
+                DrawText(TextFormat("%d", state.leftScore), screenWidth / 4, 20, 80, LIGHTGRAY);
+                DrawText(TextFormat("%d", state.rightScore), 3 * screenWidth / 4, 20, 80, LIGHTGRAY);
+                DrawText(TextFormat("Press R to restart game."), (screenWidth / 2) - (screenWidth / 4), screenHeight / 2, 60, RAYWHITE);
+                break;
+            }
         EndDrawing();
     }
 
@@ -134,7 +196,56 @@ int main(void) {
     return 0;
 }
 
-void DrawDashedLine() {
+void GameLogic(Paddle *leftPaddle, Paddle *rightPaddle, Ball *ball, GameState *state) {
+    // Input
+    if (IsKeyDown(KEY_W) && leftPaddle->rect.y > 0) {
+        leftPaddle->rect.y -= leftPaddle->speed * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_S) && leftPaddle->rect.y < screenHeight - PADDLE_HEIGHT) {
+        leftPaddle->rect.y += leftPaddle->speed * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_UP) && rightPaddle->rect.y > 0) {
+        rightPaddle->rect.y -= rightPaddle->speed * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_DOWN) && rightPaddle->rect.y < screenHeight - PADDLE_HEIGHT) {
+        rightPaddle->rect.y += rightPaddle->speed * GetFrameTime();
+    }
+
+    // Update ball position
+    ball->position.x += ball->direction.x * ball->speed * GetFrameTime();
+    ball->position.y += ball->direction.y * ball->speed * GetFrameTime();
+
+    // Ball collision with top and bottom
+    if (ball->position.y <= 0 || ball->position.y >= screenHeight - BALL_SIZE) {
+        ball->direction.y *= -1; // Reverse Y direction
+        PlaySound(state->sounds.top);
+    }
+
+    // Ball collision with paddles
+    if (CheckCollisionCircleRec(ball->position, BALL_SIZE / 2, leftPaddle->rect) || 
+        CheckCollisionCircleRec(ball->position, BALL_SIZE / 2, rightPaddle->rect)) {
+        ball->direction.x *= -1; // Reverse X direction
+        ball->speed += BALL_SPEED / 10.0;
+        PlaySound(state->sounds.hit);
+    }
+
+    // Scoring
+    if (ball->position.x < 0) {
+        PlaySound(state->sounds.edge);
+        state->rightScore++; // Right player scores
+        ResetBall(ball);
+    }
+    if (ball->position.x > screenWidth) {
+        PlaySound(state->sounds.edge);
+        state->leftScore++;  // Left player scores
+        ResetBall(ball);
+    }
+
+    if (state->leftScore == WIN_SCORE or state->rightScore == WIN_SCORE)
+        state->currentScene = GAME_OVER;
+}
+
+void DrawDashedLine(Color color) {
     Vector2 start = { (int)(screenWidth / 2), 0 };
     Vector2 end = { (int)(screenWidth / 2), screenHeight };
     float dashLength = 10.0f;
@@ -150,7 +261,7 @@ void DrawDashedLine() {
         Vector2 dashEnd = Vector2Add(dashStart, Vector2Scale(direction, dashLength));
 
         // Draw the dash
-        DrawLineEx(dashStart, dashEnd, 5, RAYWHITE);
+        DrawLineEx(dashStart, dashEnd, 5, color);
 
         // Move the total length by the dash length and gap length
         totalLength += dashLength + gapLength;
